2ae5c94d0edbd25b517705eb950104137b30b7e3
Adam <adamirzakan@gmail.com>
2026-02-13 14:46:23 +0300

    Bootstrap project

---
+++ SkyView/SkyViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyViewApp.swift
+//  SkyView
+//
+//  Created by Adam on 12.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct SkyViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
